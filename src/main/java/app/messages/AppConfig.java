14909897ee70f550617679aefe126af037cef28c
billy <billyyeung317@gmail.com>
2020-06-24 19:12:40 +0800

    finish JDBC

--- src/main/java/app/messages/AppConfig.java
+++ src/main/java/app/messages/AppConfig.java
@@ -7,13 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("app.messages")
 public class AppConfig {
-    @Bean
-    public MessageRepository messageRepository(){
-        return new MessageRepository();
-    }
 
-    @Bean
-    public MessageService messageService(){
-        return new MessageService(messageRepository());
-    }
 }
--- src/main/java/app/messages/Application.java
+++ src/main/java/app/messages/Application.java
@@ -1,12 +1,12 @@
 package app.messages;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+@SpringBootApplication
 public class Application {
     public static void main(String[] args){
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        MessageService messageService = applicationContext.getBean(MessageService.class);
-        messageService.save("hello spring");
+        SpringApplication.run(Application.class,args);
     }
 }
